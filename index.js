8a29c9909a56805062cf74dabf1f8f35ea7ae499
AK Rasel <raselak101010@gmail.com>
2023-11-06 13:00:04 +0600

    All Food items get and singel data get

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -23,6 +23,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // mongodb Collection
+    const FoodCollection = client.db('tasteBazaar').collection('foodItems');
+
+    // All Service Dta
+    app.get('/api/v1/all-food-items', async(req, res) => {
+      const cursor = FoodCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+  });
+    app.get('/api/v1/all-food-items/:_id', async(req, res) => {
+      const id = req.params._id
+      const query = {_id : new ObjectId(id)}
+      
+      const result = await FoodCollection.findOne(query)
+      res.send(result)
+  });
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
